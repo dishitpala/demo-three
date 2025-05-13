bdc5a3055443a105ee13be4b3031dbc13ca58ded
dishitpala <dishitpala@gmail.com>
2025-05-13 23:06:41 +0000

    Sync from demo-plugins

---
+++ plugins/onepress/loading-info/demo-three.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Brand Name: demo-three
+ * Return the list of plugins to load.
+ */
+
+return [
+	'wpforms-lite-1.9.5.1/wpforms.php',
+	'elementor',
+	'wordpress-seo/wp-seo.php',
+];
---
+++ plugins/onepress/loading-info/demo-two.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Brand Name: demo-two
+ * Return the list of plugins to load.
+ */
+
+return [
+	'wpforms-lite-1.9.5.1/wpforms.php',
+	'elementor',
+	'wordpress-seo/wp-seo.php',
+];
